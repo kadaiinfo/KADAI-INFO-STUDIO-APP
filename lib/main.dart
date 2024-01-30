22d5dec6e1fb487eae6adbc8ddd6d42bc7014bbf
Koki Kiyota <82688821+kkiyota63@users.noreply.github.com>
2024-01-30 20:04:05 +0900

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,96 +1,165 @@
-// Flutterの基本的なライブラリと、WebViewを扱うためのライブラリをインポート
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-// メイン関数：Flutterアプリのエントリーポイント
 void main() {
   runApp(MyApp());
 }
 
-// MyAppクラス：アプリ全体のルートウィジェット
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // アプリのテーマを設定
       theme: ThemeData(
-        // 明るさを明るいテーマに設定
         brightness: Brightness.light,
       ),
+      home: WebViewWithErrorHandling(),
+    );
+  }
+}
 
-      home: Scaffold(
-        // アプリの背景色を白に設定
-        backgroundColor: Colors.white,
-        // 以下の部分はAppBarの設定ですが、コメントアウトされているので現在は表示されません。
-        // appBar: AppBar(
-        //   title: Text(
-        //     'KADAI INFO',
-        //     style: TextStyle(color: Colors.black),
-        //   ),
-        //   backgroundColor: Colors.white,
-        // ),
-        // 本体部分。上部に30ピクセルの余白を持つWebView。
-        body: Padding(
-          padding: const EdgeInsets.only(top: 30.0),
-          child: WebViewWithErrorHandling(),
+class WebViewWithErrorHandling extends StatefulWidget {
+  @override
+  _WebViewWithErrorHandlingState createState() => _WebViewWithErrorHandlingState();
+}
+
+class _WebViewWithErrorHandlingState extends State<WebViewWithErrorHandling> {
+  late WebViewController _controller;
+  bool _isLoading = true;
+  int _currentIndex = 0;
+
+  final _urls = [
+    'https://kadaiinfo.com/',
+    'https://manaba.kic.kagoshima-u.ac.jp/',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(0.0), // AppBarの高さを設定
+        child: AppBar(
+          backgroundColor: Colors.white, // AppBarの背景色を白に設定
+          iconTheme: IconThemeData(color: Colors.black54), // アイコンの色を設定
+          elevation: 0, // AppBarの影をなくす
+          leading: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // 必要なIconButtonをここに追加
+            ],
+          ),
         ),
+      ),
+      body: _currentIndex < 2
+          ? Stack(
+              children: [
+                WebView(
+                  initialUrl: _urls[_currentIndex],
+                  onWebViewCreated: (WebViewController controller) {
+                    _controller = controller;
+                  },
+                  javascriptMode: JavascriptMode.unrestricted,
+                  onPageFinished: (String url) {
+                    setState(() {
+                      _isLoading = false;
+                    });
+                  },
+                  onPageStarted: (String url) {
+                    setState(() {
+                      _isLoading = true;
+                    });
+                  },
+                ),
+                if (_isLoading)
+                  Center(
+                    child: CircularProgressIndicator(),
+                  ),
+              ],
+            )
+          : (_currentIndex == 2 ? ContentsPage() : SettingsPage()),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black54,
+        unselectedItemColor: Colors.black54,
+        showUnselectedLabels: true,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+            if (index < 2) {
+              _controller.loadUrl(_urls[_currentIndex]);
+            }
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.article),
+            label: '記事',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Manaba',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'コンテンツ',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: '設定',
+          ),
+        ],
       ),
     );
   }
 }
 
-// WebViewを含むウィジェット。エラーハンドリングも含めたWebViewの表示を行う。
-class WebViewWithErrorHandling extends StatefulWidget {
-  @override
-  _WebViewWithErrorHandlingState createState() =>
-      _WebViewWithErrorHandlingState();
-}
-
-class _WebViewWithErrorHandlingState extends State<WebViewWithErrorHandling> {
-  late WebViewController _controller;
-  bool _isLoading = true; // WebViewの読み込み状況を表すフラグ
-
+class ContentsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        // 横方向のスワイプジェスチャを検出するウィジェット
-        GestureDetector(
-          onHorizontalDragEnd: (details) {
-            // 左方向のスワイプを検出
-            if (details.velocity.pixelsPerSecond.dx > 500) {
-              if (_controller != null) {
-                _controller.goBack(); // WebViewのページを一つ前に戻る
-              }
-            }
-          },
-          // 実際のWebViewのウィジェット
-          child: WebView(
-            initialUrl: 'https://kadaiinfo.com/',
-            onWebViewCreated: (WebViewController controller) {
-              _controller = controller;
-            },
-            javascriptMode: JavascriptMode.unrestricted,
-            onPageFinished: (String url) {
-              // ページの読み込みが完了したら、ローディングフラグをfalseにする。
-              setState(() {
-                _isLoading = false;
-              });
-            },
-            onPageStarted: (String url) {
-              // ページの読み込みを開始したら、ローディングフラグをtrueにする。
-              setState(() {
-                _isLoading = true;
-              });
-            },
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(0.0), // AppBarの高さを設定
+        child: AppBar(
+          backgroundColor: Colors.white, // AppBarの背景色を白に設定
+          iconTheme: IconThemeData(color: Colors.black54), // アイコンの色を設定
+          elevation: 0, // AppBarの影をなくす
+          leading: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // 必要なIconButtonをここに追加
+            ],
           ),
         ),
-        // ページが読み込み中の場合、ローディングインジケーターを表示する。
-        if (_isLoading)
-          Center(
-            child: CircularProgressIndicator(),
+      ),
+      
+      body: Center(
+        child: Text('Coming soon...'),
+      ),
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(0.0), // AppBarの高さを設定
+        child: AppBar(
+          backgroundColor: Colors.white, // AppBarの背景色を白に設定
+          iconTheme: IconThemeData(color: Colors.black54), // アイコンの色を設定
+          elevation: 0, // AppBarの影をなくす
+          leading: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // 必要なIconButtonをここに追加
+            ],
           ),
-      ],
+        ),
+      ),
+      body: Center(
+        child: Text('Coming soon...'),
+      ),
     );
   }
 }
