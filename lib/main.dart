ff6d13e1ffc81f9ac0591398ef04d32b48c43246
Koki Kiyota <82688821+kkiyota63@users.noreply.github.com>
2024-01-30 23:07:02 +0900

    fix setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:settings_ui/settings_ui.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
@@ -122,53 +123,64 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(0.0), // AppBarの高さを設定
+        child: AppBar(
+          backgroundColor: Colors.white, // AppBarの背景色を白に設定
+          iconTheme: IconThemeData(color: Colors.black54), // アイコンの色を設定
+          elevation: 0, // AppBarの影をなくす
+          leading: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [],
+          ),
+        ),
+      ),
       body: SettingsList(
         sections: [
-          SettingsSection(
-            title: const Text('一般'),
-            tiles: [
-              SettingsTile(
-                title: const Text('通知'),
-                leading: const Icon(Icons.notifications_active_outlined),
-                trailing: const Icon(Icons.navigate_next),
-                onPressed: (BuildContext context) {
-                  // 通知設定ページへの遷移などの処理をここに書く
-
-
-
-
-                },
-              ),
-            ],
-          ),
+          // SettingsSection(
+          //   title: const Text('一般'),
+          //   tiles: [
+          //     SettingsTile(
+          //       title: const Text('通知'),
+          //       leading: const Icon(Icons.notifications_active_outlined),
+          //       trailing: const Icon(Icons.navigate_next),
+          //       onPressed: (BuildContext context) {
+          //         // 通知設定ページへの遷移などの処理をここに書く
+          //       },
+          //     ),
+          //   ],
+          // ),
           SettingsSection(
             title: const Text('このアプリについて'),
             tiles: [
               SettingsTile(
                 title: const Text('KADAI INFOについて'),
                 trailing: const Icon(Icons.launch),
-                onPressed: (BuildContext context) {
-                  // 'KADAI INFOについて'ページへの遷移などの処理をここに書く
-                  //URLに遷移する
-  
+                onPressed: (BuildContext context) async {
+                  const url = 'https://kadaiinfo.com/about-us';
+                  if (await canLaunch(url)) {
+                    await launch(url);
+                  }
                 },
               ),
               SettingsTile(
-                title: const Text('お問合せ'),
+                title: const Text('お問い合わせ'),
                 trailing: const Icon(Icons.launch),
-                onPressed: (BuildContext context) {
-                  // お問い合わせページへの遷移などの処理をここに書く
-
+                onPressed: (BuildContext context) async {
+                  const url = 'https://kadaiinfo.com/contact';
+                  if (await canLaunch(url)) {
+                    await launch(url);
+                  }
                 },
               ),
               SettingsTile(
                 title: const Text('利用規約'),
                 trailing: const Icon(Icons.launch),
-                onPressed: (BuildContext context) {
-                  // ライセンスページへの遷移などの処理をここに書く
-
-
-
+                onPressed: (BuildContext context) async {
+                  const url = 'https://kadaiinfo.com/terms';
+                  if (await canLaunch(url)) {
+                    await launch(url);
+                  }
                 },
               ),
             ],
