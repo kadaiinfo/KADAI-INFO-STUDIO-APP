6a13b5c0dda54331deb9644e4bd368522aa354df
Koki Kiyota <82688821+kkiyota63@users.noreply.github.com>
2024-01-31 13:36:13 +0900

    add setting file

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@
 //記載した後はターミナルでflutter pub getを実行してください。
 import 'package:flutter/material.dart'; // Flutterのマテリアルデザインウィジェットをインポートします。
 import 'package:webview_flutter/webview_flutter.dart'; // WebViewを表示するためのパッケージをインポートします。
-import 'package:settings_ui/settings_ui.dart'; // 設定UIを簡単に作成するためのパッケージをインポートします。
-import 'package:url_launcher/url_launcher.dart'; // URLを開くためのパッケージをインポートします。
+import 'package:firebase_core/firebase_core.dart'; //今後通知を実装するために必要なパッケージをインポートします。
+import 'package:firebase_messaging/firebase_messaging.dart'; //今後通知を実装するために必要なパッケージをインポートします。
+
+import 'setting_page.dart'; // settingファイルをインポート
 
 void main() {
   runApp(MyApp()); // アプリケーションを起動し、MyAppをルートウィジェットとして設定します。
@@ -123,80 +125,6 @@ class _WebViewWithErrorHandlingState extends State<WebViewWithErrorHandling> {
           BottomNavigationBarItem(
             icon: Icon(Icons.settings),
             label: 'その他',
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class SettingsPage extends StatelessWidget {
-  const SettingsPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(0.0), // AppBarの高さを設定
-        child: AppBar(
-          backgroundColor: Colors.white, // AppBarの背景色を白に設定
-          iconTheme: IconThemeData(color: Colors.black54), // アイコンの色を設定
-          elevation: 0, // AppBarの影をなくす
-          leading: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [],
-          ),
-        ),
-      ),
-      body: SettingsList(
-        sections: [
-          // SettingsSection(
-          //   title: const Text('一般'),
-          //   tiles: [
-          //     SettingsTile(
-          //       title: const Text('通知'),
-          //       leading: const Icon(Icons.notifications_active_outlined),
-          //       trailing: const Icon(Icons.navigate_next),
-          //       onPressed: (BuildContext context) {
-          //         // 通知設定ページへの遷移などの処理をここに書く
-          //       },
-          //     ),
-          //   ],
-          // ),
-          SettingsSection(
-            title: const Text('このアプリについて'),
-            tiles: [
-              SettingsTile(
-                title: const Text('KADAI INFOについて'),
-                trailing: const Icon(Icons.launch),
-                onPressed: (BuildContext context) async {
-                  const url = 'https://kadaiinfo.com/about-us';
-                  if (await canLaunch(url)) {
-                    await launch(url);
-                  }
-                },
-              ),
-              SettingsTile(
-                title: const Text('お問い合わせ'),
-                trailing: const Icon(Icons.launch),
-                onPressed: (BuildContext context) async {
-                  const url = 'https://kadaiinfo.com/contact';
-                  if (await canLaunch(url)) {
-                    await launch(url);
-                  }
-                },
-              ),
-              SettingsTile(
-                title: const Text('利用規約'),
-                trailing: const Icon(Icons.launch),
-                onPressed: (BuildContext context) async {
-                  const url = 'https://kadaiinfo.com/terms';
-                  if (await canLaunch(url)) {
-                    await launch(url);
-                  }
-                },
-              ),
-            ],
           ),
         ],
       ),
