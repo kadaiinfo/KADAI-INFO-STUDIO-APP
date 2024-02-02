14dd31d64747289707fe3731b2c5a965b8e29da1
Koki Kiyota <82688821+kkiyota63@users.noreply.github.com>
2024-02-02 13:50:25 +0900

    fix

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,16 +1,48 @@
-// トップページを管理するファイルです。
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final String homeUrl = 'https://kadaiinfo.com/';
+  WebViewController? _controller; // WebViewコントローラーのインスタンス
 
   @override
   Widget build(BuildContext context) {
-    return WebView(
-      initialUrl: homeUrl,
-      javascriptMode: JavascriptMode.unrestricted,
-      // ... その他のWebView設定
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(40.0), // アプリバーの高さを設定
+        child: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0, // 影を消す
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_new, size: 20),
+            onPressed: () async {
+              if (_controller != null && await _controller!.canGoBack()) {
+                // WebViewの前のページに戻る
+                _controller!.goBack();
+              } else {
+                // WebViewで戻るページがない場合は、アプリの前の画面に戻る
+                if (Navigator.canPop(context)) {
+                  Navigator.pop(context);
+                }
+              }
+            },
+          ),
+        ),
+      ),
+      body: WebView(
+        initialUrl: homeUrl,
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller = webViewController; // コントローラーを設定
+        },
+        // ... その他のWebView設定
+      ),
     );
   }
 }
--- lib/manaba_page.dart
+++ lib/manaba_page.dart
@@ -14,22 +14,25 @@ class _ManabaPageState extends State<ManabaPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        //title: Text('Manaba'),
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios_new),
-          onPressed: () async {
-            if (_controller != null && await _controller!.canGoBack()) {
-              // WebViewの前のページに戻る
-              _controller!.goBack();
-            } else {
-              // WebViewで戻るページがない場合は、アプリの前の画面に戻る
-              if (Navigator.canPop(context)) {
-                Navigator.pop(context);
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(40.0), // アプリバーの高さを設定
+        child: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0, // 影を消す
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_new, size: 20),
+            onPressed: () async {
+              if (_controller != null && await _controller!.canGoBack()) {
+                // WebViewの前のページに戻る
+                _controller!.goBack();
+              } else {
+                // WebViewで戻るページがない場合は、アプリの前の画面に戻る
+                if (Navigator.canPop(context)) {
+                  Navigator.pop(context);
+                }
               }
-            }
-          },
+            },
+          ),
         ),
       ),
       body: WebView(
