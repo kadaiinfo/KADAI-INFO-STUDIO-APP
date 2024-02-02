36ebc29d7f370919815685507c13e61c28f4a7f1
Koki Kiyota <82688821+kkiyota63@users.noreply.github.com>
2024-02-02 13:21:48 +0900

    Arrow back

--- lib/manaba_page.dart
+++ lib/manaba_page.dart
@@ -1,16 +1,45 @@
-// manabaのページを表示するためのページ
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class ManabaPage extends StatelessWidget {
+class ManabaPage extends StatefulWidget {
+  @override
+  _ManabaPageState createState() => _ManabaPageState();
+}
+
+class _ManabaPageState extends State<ManabaPage> {
   final String manabaUrl = 'https://manaba.kic.kagoshima-u.ac.jp/';
+  WebViewController? _controller; // WebViewコントローラーのインスタンス
 
   @override
   Widget build(BuildContext context) {
-    return WebView(
-      initialUrl: manabaUrl,
-      javascriptMode: JavascriptMode.unrestricted,
-      // ... その他のWebView設定
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        //title: Text('Manaba'),
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios_new),
+          onPressed: () async {
+            if (_controller != null && await _controller!.canGoBack()) {
+              // WebViewの前のページに戻る
+              _controller!.goBack();
+            } else {
+              // WebViewで戻るページがない場合は、アプリの前の画面に戻る
+              if (Navigator.canPop(context)) {
+                Navigator.pop(context);
+              }
+            }
+          },
+        ),
+      ),
+      body: WebView(
+        initialUrl: manabaUrl,
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller = webViewController; // コントローラーを設定
+        },
+        // ... その他のWebView設定
+      ),
     );
   }
 }
